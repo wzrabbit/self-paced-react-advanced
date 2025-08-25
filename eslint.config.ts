d930ef0e6bdfae8d9447f4bf81a42af6fe4fc6ae
요술토끼 <curious.wzrabbit@gmail.com>
2025-08-25 15:03:20 +0900

    📦 chore: ESLint/Prettier 개발환경 설정 진행

---
+++ eslint.config.ts
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginPrettierRecommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-unused-vars': 'warn',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+]);
