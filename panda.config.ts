502c2c545f74be5d1c518003352d985711c7b9a1
wzrabbit <curious.wzrabbit@gmail.com>
2025-09-03 00:20:22 +0900

    ✨ feat: 프로젝트 색상들을 프리셋에 추가하고, 글로벌 css 속성들과 폰트 속성들을 추가

--- panda.config.ts
+++ panda.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from '@pandacss/dev';
-import colorPreset from './src/presets/colorPreset';
 
 export default defineConfig({
   preflight: true,
@@ -10,8 +9,46 @@ export default defineConfig({
   ],
   exclude: [],
   theme: {
-    extend: {},
+    extend: {
+      tokens: {
+        colors: {
+          primary: { value: '#ec4a0a' },
+          lighten: { value: '#f6a88a' },
+          white: { value: '#ffffff' },
+          gray: {
+            100: { value: '#f0f2f7' },
+            200: { value: '#d0d5dd' },
+            300: { value: '#667085' },
+            400: { value: '#344054' },
+            500: { value: '#000000' },
+          },
+        },
+      },
+    },
+  },
+  globalCss: {
+    'ul, li': {
+      listStyle: 'none',
+    },
+    body: {
+      fontFamily: 'Paperozi, sans-serif',
+    },
+  },
+  globalFontface: {
+    Paperozi: [
+      {
+        src: 'url("https://cdn.jsdelivr.net/gh/projectnoonnu/2408-3@1.0/Paperlogy-4Regular.woff2") format("woff2")',
+        fontWeight: 400,
+        fontStyle: 'normal',
+        fontDisplay: 'swap',
+      },
+      {
+        src: 'url("https://cdn.jsdelivr.net/gh/projectnoonnu/2408-3@1.0/Paperlogy-7Bold.woff2") format("woff2")',
+        fontWeight: 700,
+        fontStyle: 'normal',
+        fontDisplay: 'swap',
+      },
+    ],
   },
   outdir: 'styled-system',
-  presets: [colorPreset],
 });
