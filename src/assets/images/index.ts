afe4675e878f5173be76485ee8fcfc9a7568fdd2
wzrabbit <curious.wzrabbit@gmail.com>
2025-09-03 00:18:28 +0900

    ✨ feat: 버튼 이미지 / 음식점 카테고리 이미지 모음 추가

---
+++ src/assets/images/index.ts
@@ -0,0 +1,18 @@
+import addButton from './add-button.png';
+import categoryKorean from './category-korean.png';
+import categoryChinese from './category-chinese.png';
+import categoryJapanese from './category-japanese.png';
+import categoryWestern from './category-western.png';
+import categoryAsian from './category-asian.png';
+import categoryEtc from './category-etc.png';
+
+export const ADD_BUTTON_ICON = addButton;
+
+export const CATEGORY_ICONS = {
+  한식: categoryKorean,
+  중식: categoryChinese,
+  일식: categoryJapanese,
+  양식: categoryWestern,
+  아시안: categoryAsian,
+  기타: categoryEtc,
+};
