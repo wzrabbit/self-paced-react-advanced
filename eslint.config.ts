03d274b8b0dae75a10c2d04adf240c3ed794ca38
wzrabbit <curious.wzrabbit@gmail.com>
2025-09-03 00:20:39 +0900

    📦 chore: 불필요한 규칙을 해제

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,3 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
@@ -19,7 +16,7 @@ export default defineConfig([
     languageOptions: { globals: globals.browser },
     rules: {
       '@typescript-eslint/no-unused-vars': 'warn',
-      'no-unused-vars': 'warn',
+      'no-unused-vars': 'off',
       'react/react-in-jsx-scope': 'off',
     },
   },
